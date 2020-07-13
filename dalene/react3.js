f1edf2cd80a93c97c6267af74f08b26c1dcd19d4
ddewzy <ddewzy@gmail.com>
2020-07-13 16:37:49 -0700

    added react3.js

---
+++ dalene/react3.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Amplify from "aws-amplify";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { connect } from "react-redux";
+// import { withRouter } from 'react-router-dom';
+import Routes from "./Routes";
+import awsmobile from "./aws-exports";
+// import { thunkCurrentAuthenticatedUser } from "./thunks/auth";
+import Footer from "./components/Footer";
+import TopNavbar from "./components/TopNavbar";
+
+import "./App.css";
+requestAnimationFrame("typeface=quicksand");
+
+Amplify.configure(awsmobile);
+
+const theme = creteMuiTheme({
+  typography: {
+    useNextVariants: true,
+    fontSize: 16,
+    fontFamily: "'Quicksand', sans-serif;",
+  },
+  palette: {
+    primary: {
+      // main: "#6200EE"
+      // main: '#9c27b0'
+      main: "#3700B3",
+    },
+  },
+});
+
+class App extends React.Component {
+  componentDidMount() {
+    //  this.props.dispatch(thunkCurrentAuthenticatedUser());
+  }
+
+  // componentDidMount() {
+  // const { history } = this.props;
+  // console.log('history', history);
+  // }
+
+  render() {
+    const childProps = {
+      authState: this.props.authState,
+    };
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline>
+          <TopNavbar />
+          <Routes childProps={childProps} />
+          <Footer />
+        </CssBaseline>
+      </MuiThemeProvider>
+    );
+  }
+
+  handleLogout = () => {};
+}
+
+const mapStateToProps = (state) => ({
+  authState: state.authState.authState,
+});
+
+export default connect(mapStateToProps)(App);
+// export default withRouter(App);
